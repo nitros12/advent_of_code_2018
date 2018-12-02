4030afc5ffcf0602582f9053742e46836f8fd60e
Ben Simms <ben@bensimms.moe>
2018-12-02 00:50:41 +0000

    Maybe this is better idk?

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,4 @@
-use aoc_runner_derive::{aoc_generator, aoc, Runner};
+use aoc_runner_derive::{aoc, aoc_generator, Runner};
 
 #[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> Vec<i32> {
@@ -7,7 +7,6 @@ pub fn input_generator(input: &str) -> Vec<i32> {
         .map(|n| n.parse().unwrap()) // collecting Some would be slower afaik
         .collect()
 }
-
 
 #[aoc(day1, part1)]
 pub fn part1(input: &[i32]) -> i32 {
@@ -21,36 +20,36 @@ pub fn part2_stdlib(input: &[i32]) -> i32 {
     // assume not that many uniques
     let mut set = HashSet::new();
 
-    let mut freq = 0;
-
-    for i in input.into_iter().cycle() {
-        if !set.insert(freq) {
-            return freq;
-        }
-
-        freq += i;
-    }
-
-    unreachable!();
+    input
+        .into_iter()
+        .cycle()
+        .try_fold(0, |freq, n| {
+            if !set.insert(freq) {
+                Err(freq)
+            } else {
+                Ok(freq + n)
+            }
+        })
+        .unwrap_err()
 }
 
 #[aoc(day1, part2, IntHashSet)]
 pub fn part2_inthash(input: &[i32]) -> i32 {
-    use std::collections::HashSet;
     use int_hash::IntBuildHasher;
+    use std::collections::HashSet;
 
     // assume not that many uniques
     let mut set = HashSet::<_, IntBuildHasher>::default();
 
-    let mut freq = 0;
-
-    for i in input.into_iter().cycle() {
-        if !set.insert(freq) {
-            return freq;
-        }
-
-        freq += i;
-    }
-
-    unreachable!();
+    input
+        .into_iter()
+        .cycle()
+        .try_fold(0, |freq, n| {
+            if !set.insert(freq) {
+                Err(freq)
+            } else {
+                Ok(freq + n)
+            }
+        })
+        .unwrap_err()
 }
