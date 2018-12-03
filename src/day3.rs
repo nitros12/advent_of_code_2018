c4b2eca1515d1902654479a63e67d47c035e42e4
Ben Simms <ben@bensimms.moe>
2018-12-03 16:43:34 +0000

    Add day3 part2 solution

--- src/day3.rs
+++ src/day3.rs
@@ -2,9 +2,7 @@ use aoc_runner_derive::{aoc, aoc_generator, Runner};
 use regex::Regex;
 use lazy_static::lazy_static;
 use ndarray::{Array2, s};
-use ndarray_parallel::prelude::*;
-use rayon::prelude::*;
-use itertools::Itertools;
+use std::collections::HashSet;
 
 lazy_static! {
     static ref CLAIM_RE: Regex = Regex::new(r"#(?P<id>\d+) @ (?P<loffset>\d+),(?P<toffset>\d+): (?P<width>\d+)x(?P<height>\d+)").unwrap();
@@ -45,34 +43,36 @@ impl Rect {
     }
 }
 
+fn get_min_max(input: &[Rect]) -> (usize, usize, usize, usize) {
+    let mut iter = input.iter();
+
+    let first = iter.next().unwrap();
+
+    iter.fold(first.to_points(),
+              |mut minmax, rect| {
+                  let points = rect.to_points();
+
+                  if points.0 < minmax.0 {
+                      minmax.0 = points.0;
+                  }
+                  if points.1 < minmax.1 {
+                      minmax.1 = points.1;
+                  }
+                  if points.2 > minmax.2 {
+                      minmax.2 = points.2;
+                  }
+                  if points.3 > minmax.3 {
+                      minmax.3 = points.3;
+                  }
+
+                  minmax
+              })
+}
+
 #[aoc(day3, part1)]
 pub fn part1(input: &[Rect]) -> usize {
 
-    let (min_x, min_y, max_x, max_y) = {
-        let mut iter = input.iter();
-
-        let first = iter.next().unwrap();
-
-        iter.fold(first.to_points(),
-                  |mut minmax, rect| {
-                      let points = rect.to_points();
-
-                      if points.0 < minmax.0 {
-                          minmax.0 = points.0;
-                      }
-                      if points.1 < minmax.1 {
-                          minmax.1 = points.1;
-                      }
-                      if points.2 > minmax.2 {
-                          minmax.2 = points.2;
-                      }
-                      if points.3 > minmax.3 {
-                          minmax.3 = points.3;
-                      }
-
-                      minmax
-                  })
-    };
+    let (_min_x, _min_y, max_x, max_y) = get_min_max(input);
 
     let mut arr = Array2::<usize>::zeros((max_x, max_y));
 
@@ -84,4 +84,29 @@ pub fn part1(input: &[Rect]) -> usize {
 
     arr.mapv_inplace(|x| (x > 1) as usize);
     arr.sum()
+}
+
+#[aoc(day3, part2)]
+pub fn part2(input: &[Rect]) -> u16 {
+    let (_min_x, _min_y, max_x, max_y) = get_min_max(input);
+
+    let mut arr = Array2::<usize>::zeros((max_x, max_y));
+
+    let mut non_overlapping: HashSet<_> = input.iter().map(|r| r.id).collect();
+
+    for rect in input {
+        let (min_x, min_y, max_x, max_y) = rect.to_points();
+        let mut arrslice = arr.slice_mut(s![min_x..max_x, min_y..max_y]);
+
+        for &val in &arrslice {
+            if val != 0 {
+                non_overlapping.remove(&(val as u16));
+                non_overlapping.remove(&rect.id);
+            }
+        }
+
+        arrslice.fill(rect.id as usize);
+    }
+
+    *non_overlapping.iter().next().unwrap()
 }
