c946b77d21c2825bf28b0bf718df49e1c7273b08
Ben Simms <ben@bensimms.moe>
2018-12-14 10:39:35 +0000

    Add day6 part2 solution

--- src/day5.rs
+++ src/day5.rs
@@ -1,4 +1,4 @@
-use aoc_runner_derive::{aoc, aoc_generator};
+use aoc_runner_derive::aoc;
 
 use itertools::Itertools;
 use rayon::prelude::*;
--- src/day6.rs
+++ src/day6.rs
@@ -1,15 +1,11 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
-use std::{
-    fmt::Debug,
-    collections::HashMap,
-    hash::{Hash, Hasher},
-};
-use cgmath::prelude::*;
 use cgmath::Point2;
-use spade::{SpatialObject, BoundingRect, PointN, rtree::RTree};
+use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
+use spade::{rtree::RTree, BoundingRect, SpatialObject};
+use std::collections::HashMap;
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 pub struct TaxicabSpatial(pub Point2<i32>);
@@ -21,7 +17,7 @@ impl TaxicabSpatial {
 }
 
 fn taxicab(a: Point2<i32>, b: Point2<i32>) -> i32 {
-    ((a.x - b.x).abs() + (a.y - b.y).abs()).pow(2)
+    (a.x - b.x).abs() + (a.y - b.y).abs()
 }
 
 impl SpatialObject for TaxicabSpatial {
@@ -32,7 +28,7 @@ impl SpatialObject for TaxicabSpatial {
     }
 
     fn distance2(&self, point: &Self::Point) -> i32 {
-        taxicab(self.0, *point)
+        taxicab(self.0, *point).pow(2)
     }
 }
 
@@ -42,21 +38,25 @@ lazy_static! {
 
 #[aoc_generator(day6)]
 pub fn parse_input(input: &str) -> Vec<TaxicabSpatial> {
-    COORD_RE.captures_iter(input)
-        .map(|c| TaxicabSpatial::new(
-            c["x"].parse::<i32>().unwrap(),
-            c["y"].parse::<i32>().unwrap()))
+    COORD_RE
+        .captures_iter(input)
+        .map(|c| {
+            TaxicabSpatial::new(
+                c["x"].parse::<i32>().unwrap(),
+                c["y"].parse::<i32>().unwrap(),
+            )
+        })
         .collect()
 }
 
 #[derive(Default, Debug)]
 struct Region {
-    members: Vec<TaxicabSpatial>,
+    members: i32,
     infinite: bool,
 }
 
 #[aoc(day6, part1)]
-pub fn part1(inp: &[TaxicabSpatial]) -> usize {
+pub fn part1(inp: &[TaxicabSpatial]) -> i32 {
     let tree = RTree::bulk_load(inp.to_vec());
 
     let mut regions: HashMap<TaxicabSpatial, Region> = HashMap::new();
@@ -65,43 +65,52 @@ pub fn part1(inp: &[TaxicabSpatial]) -> usize {
     let lower = bounds.lower();
     let upper = bounds.upper();
 
-    for y in lower.y - 1 .. upper.y + 2 {
-        for x in lower.x - 1 .. upper.x + 2 {
+    for y in lower.y - 1..upper.y + 2 {
+        for x in lower.x - 1..upper.x + 2 {
             let point = TaxicabSpatial::new(x, y);
             let closest_n = tree.nearest_n_neighbors(&point.0, 2);
 
-            if closest_n.len() == 2 && taxicab(point.0, closest_n[0].0) == taxicab(point.0, closest_n[1].0) {
+            if closest_n.len() == 2
+                && taxicab(point.0, closest_n[0].0) == taxicab(point.0, closest_n[1].0)
+            {
                 continue;
             }
 
             let closest = closest_n[0];
 
-            let region = regions.entry(*closest)
-                                .or_default();
+            let region = regions.entry(*closest).or_default();
 
-            region.members.push(point);
+            region.members += 1;
 
-            if x < lower.x
-                || x > upper.x
-                || y < lower.y
-                || y > upper.y {
-                    region.infinite = true;
-                }
-
+            if x < lower.x || x > upper.x || y < lower.y || y > upper.y {
+                region.infinite = true;
+            }
         }
     }
 
-    regions.into_iter()
-           .filter_map(
-               |(_k, v)|
-               if v.infinite {
-                   None
-               } else {
-                   Some(v.members.len())
-               }
-           )
-           .max()
-           .unwrap()
+    regions
+        .into_iter()
+        .filter_map(|(_k, v)| if v.infinite { None } else { Some(v.members) })
+        .max()
+        .unwrap()
+}
+
+#[aoc(day6, part2)]
+pub fn part2(inp: &[TaxicabSpatial]) -> i32 {
+    let (min_x, max_x) = inp.iter().map(|c| c.0.x).minmax().into_option().unwrap();
+    let (min_y, max_y) = inp.iter().map(|c| c.0.y).minmax().into_option().unwrap();
+
+    let mut region_size = 0;
+
+    for y in min_y - 1..max_y + 2 {
+        for x in min_x - 1..max_x + 2 {
+            if inp.iter().map(|e| taxicab(e.0, Point2::new(x, y))).sum::<i32>() < 10000 {
+                region_size += 1;
+            }
+        }
+    }
+
+    region_size
 }
 
 #[cfg(test)]
